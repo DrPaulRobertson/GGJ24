a50126351477d75affb3924b01d451211cdeecf7
Paul <paul@staticmemory.co.uk>
2024-01-27 19:38:53 +0000

    added end screen

--- Scripts/End.gd
+++ Scripts/End.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var capture_input = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,14 +11,20 @@ func _ready():
 	#$GoodNumber.text = str(Global.good)
 	#$OkayNumber.text = str(Global.okay)
 	#$MissedNumber.text = str(Global.missed)
+	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Circle"):
-		if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
-			print ("Error changing scene to Game")
-			
-	if Input.is_action_just_pressed("Up"):
-		if get_tree().change_scene_to_file("res://Scenes/Menu.tscn") != OK:
-			print ("Error returning to menu")
+	if capture_input:
+		if Input.is_action_just_pressed("Circle"):
+			if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
+				print ("Error changing scene to Game")
+				
+		if Input.is_action_just_pressed("Up"):
+			if get_tree().change_scene_to_file("res://Scenes/Menu.tscn") != OK:
+				print ("Error returning to menu")
+
+
+func _on_timer_timeout():
+	capture_input = true
