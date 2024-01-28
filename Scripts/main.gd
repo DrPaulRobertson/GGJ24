c1dfcc41f4dc92238010ae00a6c96036281f1af9
Paul <paul@staticmemory.co.uk>
2024-01-28 13:09:14 +0000

    fixed particles and added second camera

---
+++ Scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var animation_idx = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Global.score > 200 and animation_idx == 0:
+		animation_idx = 1
+		#$AnimationPlayer.set_current_animation("A_Titter")
+	elif Global.score > 600 and animation_idx == 1:
+		animation_idx = 2
+		#$AnimationPlayer.set_current_animation("A_CryLaugh")
+		$Camera3D2.make_current()
